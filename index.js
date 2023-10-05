21faa6613a46e14c219572e64eab8a615f717f3b
Daniel Lira <daniel.liras87@gmail.com>
2023-10-05 12:24:27 -0400

    set up main.js and router folder

---
+++ index.js
@@ -0,0 +1,61 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+const app = express();
+export const prisma = new PrismaClient();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+// we want an auth middleware that fires before every
+// request and checks if theres a token and checks if that token is valid and grabs the user info and stores it in req.user
+// logged in back end? req.user
+
+app.use(async (req, res, next) => {
+  // check if theres an auth token in header and console it
+  try {
+    if (!req.headers.authorization) {
+      return next();
+    }
+
+    const token = req.headers.authorization.split(" ")[1];
+
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      return next();
+    }
+    delete user.password;
+    req.user = user;
+    next();
+  } catch (error) {
+    res.send({ success: false, error: error.message });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send({
+    success: true,
+    message: "Welcome to the Reddit backend server!",
+  });
+});
+
+app.use((req, res) => {
+  res.send({ success: false, error: "No route found." });
+});
+
+app.use((error, req, res, next) => {
+  res.send({ success: false, error: error.message });
+});
+
+app.listen(3000, () => console.log("Server is up!"));
